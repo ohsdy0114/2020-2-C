45dc8c905aa1dc6c0c4b5ed00c9033311e50e4e6
[ohs] <[ohsdy0114@gmail.com]>
2020-12-09 18:34:50 +0900

    Create pointarray.c

---
+++ code/ch09/pointarray.c
@@ -0,0 +1,30 @@
+//file: pointarray.c
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+#define SIZE 3
+
+int main(void)
+{
+	//포인터 배열 변수 선언
+	int* pary[SIZE] = { NULL };
+	int a = 10, b = 20, c = 30;
+
+	pary[0] = &a;
+	pary[1] = &b;
+	pary[2] = &c;
+
+	for (int i = 0; i < SIZE; i++)
+		printf("*pary[%d] = %d\n", i, *pary[i]);
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		scanf("%d", pary[i]);
+		printf("%d, %d, %d\n", a, b, c);
+	}
+
+
+	return 0;
+}
