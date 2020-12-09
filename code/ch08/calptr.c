3503934a75a13750c78e25f8ce754ffc7385830e
[ohs] <[ohsdy0114@gmail.com]>
2020-12-09 18:33:07 +0900

    Create calptr.c

---
+++ code/ch08/calptr.c
@@ -0,0 +1,22 @@
+//file: calptr.c
+
+#include <stdio.h>
+
+int main(void)
+{
+	char* pc = (char*)100;
+	int* pi = (int*)100;
+	double* pd = (double*)100;
+	pd = 100;
+
+	printf("%u %u %u\n", (int)(pc - 1), (int)pc, (int)(pc + 1));
+	printf("%u %u %u\n", (int)(pi - 1), (int)pi, (int)(pi + 1));
+	printf("%u %u %u\n", (int)(pd - 1), (int)pd, (int)(pd + 1));
+
+	//printf("%p %p %p\n", (int)(pc - 1), (int)pc, (int)(pc + 1));
+	//printf("%p %p %p\n", (int)(pi - 1), (int)pi, (int)(pi + 1));
+	//printf("%p %p %p\n", (int)(pd - 1), (int)pd, (int)(pd + 1));
+
+
+	return 0;
+}
